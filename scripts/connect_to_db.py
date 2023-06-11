52cf1b9186245acb2c6d82b1f11527e1cbe48252
[hollow] <73607555+hollowed1337@users.noreply.github.com>
2023-06-11 14:40:51 +0500

    Update connect_to_db.py

--- scripts/connect_to_db.py
+++ scripts/connect_to_db.py
@@ -10,8 +10,8 @@ DB_NAME = os.getenv('DB_NAME')
 URL = os.getenv('URL')
 try:
     def connect_to_batabase():
-        conn = psycopg2.connect(f'postgresql://{DB_USER}:{DB_PASSWD}@{URL}:5432/{DB_NAME}') #основная бд
-        #conn = psycopg2.connect(f'postgresql://{DB_USER}:{DB_PASSWD}@database:5432/{DB_NAME}') #тестовая бд
+        #conn = psycopg2.connect(f'postgresql://{DB_USER}:{DB_PASSWD}@{URL}:5432/{DB_NAME}') #основная бд
+        conn = psycopg2.connect(f'postgresql://{DB_USER}:{DB_PASSWD}@database:5432/{DB_NAME}') #тестовая бд
         return conn
 except:
     print('Невозможно подключиться к базе данных')
