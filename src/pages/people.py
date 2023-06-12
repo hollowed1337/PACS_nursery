570f3359630fef514755a716402cad81f5d01841
[hollow] <73607555+hollowed1337@users.noreply.github.com>
2023-06-12 08:26:36 +0300

    Update people.py

--- src/pages/people.py
+++ src/pages/people.py
@@ -3,6 +3,7 @@ from fastapi.templating import Jinja2Templates
 from src.auth.router import akb_soc
 from src.database import SessionLocal
 from src.router import rout_People, rout_Role
+from src.auth.router import akb_soc
 
 def get_db():
 
@@ -22,32 +23,32 @@ templates_people = Jinja2Templates(directory="src/templates/people")
 
 
 @router.get("/peoples/create") # 
-def create_people_page(request: Request, roles = Depends(rout_Role.read_roles)):
+def create_people_page(request: Request, roles = Depends(rout_Role.read_roles), peoples = Depends(rout_People.read_peoples), auth = Depends(akb_soc)):
      
-    return templates_people.TemplateResponse("people_create.html", {"request": request, "roles": roles})
+    return templates_people.TemplateResponse("people_create.html", {"request": request, "roles": roles, "auth": auth, "peoples": peoples})
 
 @router.get("/peoples")
-def get_peoples_page(request: Request, peoples = Depends(rout_People.read_peoples), roles = Depends(rout_Role.read_roles)):
+def get_peoples_page(request: Request, peoples = Depends(rout_People.read_peoples), roles = Depends(rout_Role.read_roles), auth = Depends(akb_soc)):
     
-    return templates_people.TemplateResponse("peoples.html", {"request": request, "peoples": peoples,  "roles": roles})
+    return templates_people.TemplateResponse("peoples.html", {"request": request, "peoples": peoples,  "roles": roles, "auth": auth})
 
 
 @router.get("/peoples/role={role_id}")
-def get_peoples_by_role_page(request: Request, peoples = Depends(rout_People.read_peoples_by_role), roles = Depends(rout_Role.read_roles)):
+def get_peoples_by_role_page(request: Request, peoples = Depends(rout_People.read_peoples_by_role), roles = Depends(rout_Role.read_roles), auth = Depends(akb_soc)):
     
-    return templates_people.TemplateResponse("peoples.html", {"request": request, "peoples": peoples, "roles": roles})
+    return templates_people.TemplateResponse("peoples.html", {"request": request, "peoples": peoples, "roles": roles, "auth": auth})
 
 @router.get("/people/{people_id}")
-def get_people_page(request: Request, people = Depends(rout_People.get_people)):
+def get_people_page(request: Request, people = Depends(rout_People.get_people), peoples = Depends(rout_People.read_peoples), auth = Depends(akb_soc)):
     
-    return templates_people.TemplateResponse("people.html", {"request": request, "people": people})
+    return templates_people.TemplateResponse("people.html", {"request": request, "people": people, "auth": auth, "peoples": peoples})
 
 
 
 @router.get("/people/edit/{people_id}") 
-def edit_people_page(request: Request, people = Depends(rout_People.get_people), roles = Depends(rout_Role.read_roles)):
+def edit_people_page(request: Request, people = Depends(rout_People.get_people), roles = Depends(rout_Role.read_roles), peoples = Depends(rout_People.read_peoples), auth = Depends(akb_soc)):
     
-    return templates_people.TemplateResponse("people_edit.html", {"request": request, "people": people, "roles": roles})
+    return templates_people.TemplateResponse("people_edit.html", {"request": request, "people": people, "roles": roles, "auth": auth, "peoples": peoples})
 
 
 @router.get("/people/delete/{people_id}")
